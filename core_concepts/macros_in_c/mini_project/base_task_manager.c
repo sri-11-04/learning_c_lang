9ca7ef72c335d79b0607c126f532cc34713c9ff2
sri-11-04 <srianna6778@gmail.com>
2025-09-22 00:24:15 +0530

    new update

--- core_concepts/macros_in_c/mini_project/base_task_manager.c
+++ core_concepts/macros_in_c/mini_project/base_task_manager.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-static void increment(TaskManager *self);
-static void add_task(TaskManager *self,Task *task);
-static void view_task(TaskManager *self);
-
 
 typedef enum{
-    COMPLETED,
-    INCOMPLETED
+    INCOMPLETE,
+    COMPLETED
 }Status;
 
 typedef struct{
@@ -18,18 +14,75 @@ typedef struct{
 
 typedef Task Data[100];
 
-typedef struct{
-    Data task_arr;
+struct TaskManager{
+    int size;
+    int capacity;
     int serial;
-    void (*increment)(TaskManager *self);
-}TaskManager;
+    Data task_arr;
+    int (*increment)(struct TaskManager *self);
+    _Bool (*add_task)(struct TaskManager *self, Task *task);
+    void (*view_task)(struct TaskManager *self);
+};
 
-
+static int increment(struct TaskManager *self);
+static _Bool add_task(struct TaskManager *self,Task *task);
+static void view_task(struct TaskManager *self);
 
 int main(void){
-    TaskManager task_manager;
-
+    Data arr;
+    struct TaskManager task_manager ;
+    task_manager.size=0;
+    task_manager.capacity=100;
+    task_manager.serial=1;
+    *task_manager.task_arr = *arr;
+    task_manager.increment = increment;
+    task_manager.add_task = add_task;
+    task_manager.view_task = view_task;
+    // {0,100,1,arr,increment,add_task,view_task};
+    int choice;
+    Task task;
+    _Bool run = 1;
+    while(run){
+        printf("1. Add Task\n2. View Tasks\n3. Exit\nchoose: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter task name: ");
+                scanf(" %99[^\n]",task.name);
+                printf("[%d] %s %s\n",__LINE__,__TIME__,task.name);
+                task.status = INCOMPLETE;
+                if (task_manager.add_task(&task_manager,&task)){
+                    printf("Task added!\n");
+                }else{
+                    printf("No space in memory to add task!\n");
+                    run = 0;
+                }
+                break;
+            case 2:
+                task_manager.view_task(&task_manager);
+                break;
+            case 3:
+                printf("Goodbye!!. Have a nice day\n");
+                run=0;
+                break;
+            default:
+                printf("Invalid input\n");
+        }
+        putchar('\n');
+    }
     return 0;
 }
 
-static void increment(TaskManager *self) {self->serial+=1;}
+static int increment(struct TaskManager *self) {return self->serial++;}
+static _Bool add_task(struct TaskManager *self,Task *task){
+    if (self->size == self->capacity) return 0;
+    task->id = self->increment(self);
+    *(self->task_arr+self->size) = *task;
+    self->size++;
+    return 1;
+}
+static void view_task(struct TaskManager *self){
+    for (int i = 0;i<self->size;i++){
+        printf("[%d] %s - %s\n",(self->task_arr+i)->id,(self->task_arr+i)->name,((self->task_arr+i)->status) ? "Completed" : "Pending");
+    }
+}
