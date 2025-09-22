82bdea701c34b0a05d7675667d880eb40def371a
sri-11-04 <srianna6778@gmail.com>
2025-09-23 00:20:32 +0530

    changes 23-09-2025 12:20 am

--- core_concepts/file_handling/beginner/challenge3_copy_file_content.c
+++ core_concepts/file_handling/beginner/challenge3_copy_file_content.c
@@ -18,40 +18,63 @@ Use fopen in "r" mode for reading and "w" mode for writing.
 Read using either fgetc (character by character) or fgets (line by line).
 
 Handle error cases if either file cannot be opened.
+
+Extra:
+
+    Display how many characters were copied in total.
 */
 
 #include <stdio.h>
 #include <string.h>
 
 #define READ_FILE "./output.txt"
-#define WRITE_FILE "./copy.txt"
+#define WRITE_CHAR_FILE "./copy_char.txt"
+#define WRITE_LINE_FILE "./copy_line.txt"
+#define MAX_SIZE 100
 
-static void using_fgets_and_fputs(FILE *read_file,FILE *write_file);
-static void using_fgetc_and_fputc(FILE *read_file,FILE *write_file);
+static void using_fgets_and_fputs(int *count,FILE *read_file,FILE *write_file);
+static void using_fgetc_and_fputc(int *count,FILE *read_file,FILE *write_file);
 
 int main(void){
-    FILE *file_read = fopen(READ_FILE,"r"),*file_write_s = fopen(WRITE_FILE,"w"),
-        *file_write_c = fopen(WRITE_FILE,"w");
+    FILE *file_read_s = fopen(READ_FILE,"r"),*file_write_s = fopen(WRITE_LINE_FILE,"w"),
+        *file_read_c = fopen(READ_FILE,"r"), *file_write_c = fopen(WRITE_CHAR_FILE,"w");
+    int total_char_s = 0,total_char_c=0;
     
-    if (!(file_read)){
+    if (!(file_read_c || file_read_s)){
         printf("Complete your challenge 1 before proceeding here\n");
         return 1;
     }else if(!(file_write_c || file_write_s)){
-        printf("Error while creating the file %s\n",WRITE_FILE);
+        printf("Error while creating the file %s, %s\n",WRITE_CHAR_FILE,WRITE_LINE_FILE);
         return 1;
     }
 
-    using_fgets_and_fputs();
-    using_fgetc_and_fputc();
-    fclose(file_read);
+    using_fgets_and_fputs(&total_char_s,file_read_s,file_write_s);
+    using_fgetc_and_fputc(&total_char_c,file_read_c,file_write_c);
+
+    printf("Total count char_by_char: %d, line_by_line: %d\n",total_char_c,total_char_s);
     return 0;
 }
 
 
-static void using_fgets_and_fputs(FILE *read_file,FILE *write_file){
+static void using_fgets_and_fputs(int *count,FILE *read_file,FILE *write_file){
+    char string[MAX_SIZE];
 
+    while((fgets(string,MAX_SIZE,read_file) != NULL)){
+        fputs(string,write_file);
+        *count+=strlen(string);
+    }
+    printf("Success coping contents from %s to %s through fgets and fputs\n",READ_FILE,WRITE_LINE_FILE);
+    fclose(read_file);
+    fclose(write_file);
 }
 
-static void using_fgetc_and_fputc(FILE *read_file,FILE *write_file){
-
+static void using_fgetc_and_fputc(int *count,FILE *read_file,FILE *write_file){
+    char c;
+    while((c = fgetc(read_file)) != EOF){
+        fputc(c,write_file);
+        *count = *count+1;
+    }
+    printf("Success coping contents from %s to %s through fgetc and fputc\n",READ_FILE,WRITE_CHAR_FILE);
+    fclose(read_file);
+    fclose(write_file);
 }
